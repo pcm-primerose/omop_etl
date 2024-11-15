7c930808e39c317b1b1b710c2f6d6d554d5a88f2
Gabrielstav <gabrielstav@yahoo.no>
2024-11-15 19:36:17 +0100

    added basic structure outline

--- src/tables/person.py
+++ src/tables/person.py
@@ -1,1 +1,12 @@
-import sqlalchemy
+import sqlalchemy as sa
+from ..sql.sql_logic import process_age 
+
+class ProcessPerson: 
+
+    def __init__(self, source_data, output_data) -> None:
+        self.source_data = source_data
+        self.output_data = output_data
+
+    def run() -> None: 
+        # run private methods (if any) to process person 
+        pass 
