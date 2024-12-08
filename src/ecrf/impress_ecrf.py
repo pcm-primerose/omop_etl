33c601f45eabdcca2712ca5db2ae30404e9f4df9
Gabrielstav <gabrielstav@yahoo.no>
2024-12-08 16:04:31 +0100

    comments

--- src/ecrf/impress_ecrf.py
+++ src/ecrf/impress_ecrf.py
@@ -1,18 +1,54 @@
 import pandas as pd
 import openpyxl as px
-im
 from dataclasses import dataclass
 from pydantic import BaseModel
+from pathlib import Path 
+
+# TODO this'll just be for impress for now (maybe ever)
+# so just extract data as is, combine and write as one csv? 
+# we assume the disjoint input (csv) from 1753 not the combined format 
+# and then we can just run this script on 1753 to get the data we want, encrypt it, export and import to 2828 
+# and treat it as any other data source 
 
 class ExtractECRF:
+    # entry point for all ecrf processing 
     # later have some entry point that calls the correct classes etc accoring to what trial etc
     pass
 
-def process_impress(csv_mode: bool = True) -> None:
-    # call correct functions on impress data
-    # make configurable for csv files or for xlsx
-    # and pass correct arg to funcs
-    # or just wait intil we have python on p28 and use the xlsx eCRF there
 
-def extract_adverse_events(input: px.sheet | Path) ->
+class InputController:
+
+    def __init__(self, ecrf_path: Path): 
+        self.ecrf_path = ecrf_path
+
+    def resolve_input(self): 
+        # given a file or dir
+        # return correct data as validated 
+        # dataframe to extraction functions 
+        pass 
+
+class ImpressEcrf: 
+    # run everything based on config 
+    # resolve input, pass config/meta state 
+    # and invoke each subclass/method to run extraction
+    # and return structured objects 
+    pass 
+
+# just easiest to grab everything per sheet at once, but we can keep it as modular as possible 
+# so we can start with COH 
+
+def extract_adverse_event_name(input: px.sheet | Path): 
+    # not sure if we actually get csv or xlsx yet, but for now need to use csv files from p1753 
+    # since nothing is installed yet on p2828
+    pass 
+
+def extract_cohort_name(): 
+    # do eveything here
+    # - find path, open file, extract name, return 
+    # or just dependancy inject a dataframe so this works regardless of xlsx or csv 
+    # so this will get the correct sheet the data is in (e.g. COH) and then we just assume the correct dat is present
+    # extract it and then 
+
+    # drop patients with no cohort status 
+    # use patient impress ID as patient ID
 
--- src/harmonization/harmonizer.py
+++ src/harmonization/harmonizer.py
@@ -1,6 +1,7 @@
 from pydantic import BaseModel
 from pathlib import Path
 from dataclasses import dataclass
+from typing import Optional, TypeVar
 
 # have some main entry point to take X extracted ecrf files and standardize, then harmonize by instantiating
 # dataclasses mirroring variable list struct and have meta for which trials are where, so we can just call this on all data
@@ -30,3 +31,66 @@ class HarmonizedVariables(BaseModel):
     AEs: AdverseEvents
     # etc
     pass
+
+# TODO Need to extract from eCRF before this, 
+# as each abstract var contains many vars and I don't know their types 
+# and level of nesting etc
+
+
+
+@dataclass 
+class MedicalHistory: 
+    # for instance what will this contain? 
+    pass 
+
+
+# almost all of these are nested and need separate dataclasses 
+# so we can just nest adverse events to one field per patient
+# and probably need further nesting in each subclass as well 
+# as this is per patient 
+@dataclass 
+class PrimeRoseVariables:
+    cohort_name: str 
+    trial: str 
+    refined_trial_id: str 
+    tumor_type: str 
+    study_drug_1: str
+    study_drug_2: str 
+    biomarker: str 
+    age: int 
+    sex: str 
+    ecog_who_performance_status_basline: str #? 
+    medical_history: MedicalHistory #? 
+    previous_treatment_lines: str #?
+    date_of_death: Optional["date_type_here"] = "NaN type here"
+    date_lost_to_follow_up: Optional["date_type_here"] = "NaN type here"
+    evaluability: bool # actually yes no but idk 
+    date_of_treatment_start: "date_type_here"
+    date_of_treatment_cycle_start: "date_type_here"
+    date_of_treatment_cycle_end: "date_type_here"
+    date_of_last_treatment_cycle_start: "date_type_here"
+    date_of_end_of_treatment: Optional["date_type_here"] = "NaN type here"
+    dose_delivered_milligrams: int # nested, should make drug class 
+    all_concomitant_medication: str # composition here 
+    adverse_event: bool 
+    adverse_event_grade: int 
+    adverse_event_ctcae_term: str 
+    date_of_adverse_event_start: str
+    date_of_adverse_event_end: str 
+    adverse_event_outcome: str 
+    adverse_event_management: str 
+    total_number_of_adverse_events: int 
+    serious_adverse_event: bool 
+    adverse_event_related_to_treatment: bool # or yes no unknown 
+    adverse_event_expectedness: str # nested, susar and what drug etc 
+    tumor_type_assessment: str 
+    date_of_assessment: str 
+    baseline_evaluation: str # actually many vars again so need separate struct 
+    change_from_baseline: str # idk what we landed on but probably absolute measruments etc 
+    response_assessment: str 
+    patient_date_of_end_of_treatment: str #so patient decided to end? bool + date type 
+    date_of_end_of_treatment_reasons: str 
+    best_overall_response: str 
+    clinical_benfit_week16: str 
+    quality_of_life_assessment: str #did we decide to just drop this? 
+    
