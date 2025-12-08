87fbddf9e4b7acf6ae3e9ab32f11813d1228ca16
Gabrielstav <49963039+Gabrielstav@users.noreply.github.com>
2025-12-08 14:16:11 +0100

    tests: added query extractor base test

--- tests/semantic_mapping/conftest.py
+++ tests/semantic_mapping/conftest.py
@@ -2,11 +2,19 @@ from typing import List
 import pytest
 from pathlib import Path
 import polars as pl
+import datetime as dt
+
+from omop_etl.harmonization.datamodels import (
+    MedicalHistory,
+    TumorType,
+    Patient,
+)
 
 from omop_etl.semantic_mapping.models import (
     Query,
     OmopDomain,
     QueryTarget,
+    FieldConfig,
 )
 
 
@@ -61,3 +69,60 @@ def queries() -> List[Query]:
         ),
     ]
     return queries
+
+
+@pytest.fixture
+def patients() -> List[Patient]:
+    patient_1 = Patient(
+        patient_id="1",
+        trial_id="test",
+    )
+    patient_2 = Patient(
+        patient_id="2",
+        trial_id="test",
+    )
+
+    tumor_1 = TumorType(patient_id="1")
+    tumor_2 = TumorType(patient_id="2")
+    medical_history_1 = [MedicalHistory(patient_id="1")]
+    medical_history_2 = [MedicalHistory(patient_id="2")]
+
+    # scalars
+    patient_1.age = 1
+    patient_1.evaluable_for_efficacy_analysis = False
+    patient_1.treatment_start_date = dt.date(1999, 1, 1)
+    patient_1.sex = "M"
+    patient_2.age = 100
+    patient_2.evaluable_for_efficacy_analysis = True
+    patient_2.treatment_start_date = dt.date(2999, 2, 2)
+    patient_2.sex = "F"
+
+    # singleton
+    patient_1.tumor_type = tumor_1
+    patient_1.tumor_type.main_tumor_type = "tumor_1"
+    patient_2.tumor_type = tumor_2
+    patient_2.tumor_type.main_tumor_type = "tumor_2"
+
+    # collection
+    patient_1.medical_histories = medical_history_1
+    for mh in patient_1.medical_histories:
+        mh.term = "medical_history_1"
+    patient_2.medical_histories = medical_history_2
+    for mh in patient_2.medical_histories:
+        mh.term = "medical_history_2"
+
+    print(f"patient 1: {patient_1}")
+
+    return [patient_1, patient_2]
+
+
+@pytest.fixture
+def configs() -> List[FieldConfig]:
+    tumor_config = FieldConfig(
+        name="tumor type main", field_path=("tumor_type", "main_tumor_type"), target=QueryTarget([OmopDomain.CONDITION])
+    )
+    medical_history_config = FieldConfig(
+        name="medical histories", field_path=("medical_histories", "term"), target=QueryTarget([OmopDomain.CONDITION, OmopDomain.PROCEDURE])
+    )
+
+    return [tumor_config, medical_history_config]
