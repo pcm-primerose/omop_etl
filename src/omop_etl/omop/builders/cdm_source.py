1189457f4cc9076fc3f9d368fc9974b39686e5b7
Gabrielstav <49963039+Gabrielstav@users.noreply.github.com>
2026-09-14 14:31:41 +0200

    feat: bumped to cdm 55

--- src/omop_etl/omop/builders/cdm_source.py
+++ src/omop_etl/omop/builders/cdm_source.py
@@ -17,16 +17,17 @@ class CdmSourceBuilder:
         cdm_concept = self.concepts.resolve("cdm", domains={"Metadata"})
         cdm_version_concept_id = int(cdm_concept[0].concept_id) if cdm_concept else 0
 
+        # todo: improve this later
         return CdmSourceRow(
             cdm_source_name="PRIME-ROSE OMOP ETL",
             cdm_source_abbreviation="PR-OMOP-ETL",
             cdm_holder="PRIME-ROSE",
             source_description="PRIME-ROSE OMOP ETL",
-            source_documentation_reference="PRIME-ROSE github",
+            source_documentation_reference="PRIME-ROSE github docs",
             cdm_etl_reference=None,
             source_release_date=dt.date.today(),
             cdm_release_date=dt.date.today(),
-            cdm_version="v5.4",
+            cdm_version="v5.5",
             cdm_version_concept_id=cdm_version_concept_id,
             vocabulary_version=self.athena_version,
         )
